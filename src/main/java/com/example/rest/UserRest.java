c3db63288f5c000ef856b7bd76188698053ffef8
deddy syuhendra <fransiscus.deddy@gmail.com>
2018-09-02 09:51:38 +0700

    a

---
+++ src/main/java/com/example/rest/UserRest.java
@@ -0,0 +1,16 @@
+package com.example.rest;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "api")
+public class UserRest {
+
+    @RequestMapping(value = "user")
+    public String getUser(){
+        return "userrr";
+    }
+
+}
