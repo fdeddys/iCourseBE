c9f3aaf8fa2d1f52bfec11a0a93ba67c68c90084
deddy syuhendra <fransiscus.deddy@gmail.com>
2018-09-02 10:35:08 +0700

    a

--- src/main/java/com/ddabadi/rest/UserRest.java
+++ src/main/java/com/ddabadi/rest/UserRest.java
@@ -1,6 +1,8 @@
 package com.ddabadi.rest;
 
 
+import com.ddabadi.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "api")
 public class UserRest {
 
+    @Autowired private UserServiceImpl userService;
     @RequestMapping(value = "user")
     public String getUser(){
-        return "userrr";
+        return userService.findOne(1L).toString();
     }
 
 }
